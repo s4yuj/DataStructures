f0bdb15b339b9c3682d39b47cc9edd1a67609709
sayuj <sayujs@students.cs.ubc.ca>
2023-06-19 08:27:24 +0530

    add, traversal (?)

--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -4,50 +4,41 @@ public class Tree {
 
     Node start;
 
-    public Tree(int object) {
-        start = new Node(object);
+    public Tree() {
     }
 
-    // CONSTRAINT: all elements in the tree are unique
-    public void add(int object) {
-        Node pointer = start;
-        Node next = null;
-        boolean atEnd = false;
-        while (!atEnd) {
-            if (object > pointer.object) {
-                // go right
-                next = pointer.right;
-            } else if (object < pointer.object) {
-                // go left
-                next = pointer.left;
-            } else {
-                System.out.println("node already exists");
-            }
-            if (next == null) {
-                next = new Node(object); //assign object to this position
-                System.out.println("node added");
-                atEnd = true; //break out of loop/
-            }
+    public Node getStart() {
+        return start;
+    }
+
+    // CONSTRAINT: all elements are unique and
+    //              existing binary search tree is ordered
+
+    public boolean add(int value) {
+        if (start == null) {
+            start = new Node(value);
+            return true;
+        } else {
+            return start.add(value);
         }
     }
 
-    public void delete(int object) {
-        Node pointer = start;
-        Node next = null;
-        boolean atEnd = false;
-        while (!atEnd) {
-            if (pointer.object == object) { //object found
-                //delete object (!!!)
-                // move node children (!!!)
-                atEnd = true;
-            }
 
-            if (object > pointer.object) {
-                next = pointer.right;
-            } else if (object < pointer.object) {
-                next = pointer.left;
-            }
+    public void preOrderPrint(Node node) {
+        if (node == null) {
+            return;
         }
+        System.out.println(node.object);
+        preOrderPrint(node.left);
+        preOrderPrint(node.right);
     }
 
+    public void postOrderPrint(Node node) {
+        if (node == null) {
+            return;
+        }
+        postOrderPrint(node.left);
+        postOrderPrint(node.right);
+        System.out.println(node.object);
+    }
 }
